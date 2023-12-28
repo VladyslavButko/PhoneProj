7ecf56fdb4678ec14ade644d770f71cbe64b1fb7
VladyslavButko <91527015+VladyslavButko@users.noreply.github.com>
2023-12-29 01:50:20 +0200

    add proxy

--- Data/Program.cs
+++ Data/Program.cs
@@ -1,11 +1,7 @@
-﻿using MySql.Data.MySqlClient;
-using PhoneMysql.Data.DAO.Interface;
+﻿using PhoneMysql.Data.DAO.Interface;
 using PhoneMysql.Data.DAO.MySQLImplementation;
 using PhoneMysql.Data.DB;
 using PhoneMysql.Data.Entities;
-using PhoneMysql.Data.FactoryMethode;
-using PhoneMysql.Data.Memento;
-using PhoneMysql.Data.Observer;
 
 namespace PhoneMysql.Data
 {
@@ -20,64 +16,16 @@ namespace PhoneMysql.Data
 
             DBConnection dbConnection = DBConnection.GetInstance(server, database, userId, password);
 
-            // DAO для MobilePhone
-            DAO<MobilePhone> mobilePhoneDAO = DAOFactory.GetDAO<MobilePhone>(DAOFactory.DAOType.MobilePhone, dbConnection);
-
-            // DAO для Order
-            DAO<Order> orderDAO = DAOFactory.GetDAO<Order>(DAOFactory.DAOType.Order, dbConnection);
-
-            //DAO для User
-            DAO<User> userDAO = DAOFactory.GetDAO<User>(DAOFactory.DAOType.User, dbConnection);
-
-            UserObserver userAddedNotifier = new UserAddedNotifier();
-            ((MySQLUserDAO)userDAO).userAddedObserver(userAddedNotifier);
-
-            //Додавання користувача за допомогою фабрики та білдера
-            User newUser = new User.Builder("John@example.com", 2, "password123")
-                .setName("John")
-                .setSurname("Doe")
-                .Build();
-
-            //Додавання до БД
-            userDAO.add(newUser);
-
-            User foundUser = ((MySQLUserDAO)userDAO).getUserByEmail("John@example.com");
-
-            if (foundUser != null)
-            {
-                Console.WriteLine($"User found: Id: {foundUser.Id}, Name: {foundUser.Name} Email: {foundUser.Email}");
-            }
-            else
-            {
-                Console.WriteLine("User not found.");
-
-            }
+            Order orderToDelete = new Order { Id = 123 };
+            OrderDAO orderDAO = new MySQLOrderDAO(dbConnection);
 
 
-            MobilePhone phone = new MobilePhone.Builder("Apple", "iPhone 13", "128 GB", 
-                "4 GB", 6.1, "12 + 12", 2815, 799.0).Build(); //Створення телефону за допомогою Builder
+            int userRoleId = 2; //додамо роль, для прикладу
 
-            // Створення списку для змін
-            List<PhoneMemento> changes = new List<PhoneMemento>();
+            //створення прокси OrderDAO
+            OrderDAO orderProxyDAO = new OrderProxyDAO(orderDAO, userRoleId);
 
-            //Збереження зміни
-            changes.Add(phone.SaveState());
-
-            // Змінюємо значення поля
-            phone.price = 999.0;
-
-            // Зберегаємо стан
-            changes.Add(phone.SaveState());
-
-            // Відміна стану
-            if (changes.Count > 1)
-            {
-                phone.RestoreState(changes[changes.Count - 2]);
-                changes.RemoveAt(changes.Count - 1);
-            }
-
-
-
+            orderProxyDAO.delete(orderToDelete);
         }
     }
 }
---
+++ Data/Proxy/UserProxyDAO.cs
@@ -0,0 +1,67 @@
+﻿using PhoneMysql.Data.DAO.Interface;
+using PhoneMysql.Data.Entities;
+using PhoneMysql.Data.DB;
+
+namespace PhoneMysql.Data.DAO.MySQLImplementation
+{
+        public enum UserRole //Можливі ролі
+        {
+            Admin = 1,
+            User = 2
+        }
+
+        public class OrderProxyDAO : OrderDAO
+        {
+
+            private readonly DBConnection dbConnection;
+            private readonly User currentUser;
+
+            private readonly OrderDAO orderDAO;
+            private readonly int userRoleId; // Роль користувача
+
+            public OrderProxyDAO(OrderDAO dao, int userRoleId)
+            {
+                this.orderDAO = dao;
+                this.userRoleId = userRoleId;
+            }
+
+            public int add(Order order)
+            {
+                return orderDAO.add(order);
+            }
+
+            public int delete(Order order)
+            {
+                //Метод тільки для адміна
+                if (userRoleId == (int)UserRole.Admin)
+                {
+                    return orderDAO.delete(order);
+                }
+                else
+                {
+                    Console.WriteLine("Access denied. Only admins can delete orders.");
+                    return -1;
+                }
+            }
+
+            public Order getOrderById(int id)
+            {
+                return orderDAO.getOrderById(id);
+            }
+
+            public int update(Order order)
+            {
+                //Метод тільки для адміна
+                if (userRoleId == (int)UserRole.Admin)
+                {
+                    return orderDAO.update(order);
+                }
+                else
+                {
+                    Console.WriteLine("Access denied. Only admins can update orders.");
+                    return -1;
+                }
+
+            }
+        }
+}
